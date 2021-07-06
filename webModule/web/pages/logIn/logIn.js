07c682c937f0bd19750b383c22421d8906d6be26
TomBer101 <74832275+TomBer101@users.noreply.github.com>
2021-07-06 15:30:42 +0300

    first

---
+++ webModule/web/pages/logIn/logIn.js
@@ -0,0 +1,21 @@
+$(function ()
+{
+    $('loginForm').submit(function ()
+    {
+        $.ajax({
+            data: $(this).serialize(),
+            url: this.action,
+            timeout: 2000,
+            error: function (errorObject)
+            {
+                console.error("Failed to login!");
+                $("#error-placeholder").append(errorObject.responseText)
+            },
+            success: function(nextPageUrl)
+            {
+                window.location.replace(nextPageUrl);
+            }
+        });
+        return false;
+    });
+});
