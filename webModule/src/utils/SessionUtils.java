ec7eb8fbe1feb706f13b5012e8f7caeaa5e7b66a
TomBer101 <74832275+TomBer101@users.noreply.github.com>
2021-07-12 17:22:19 +0300

    broker home page js

--- webModule/src/utils/SessionUtils.java
+++ webModule/src/utils/SessionUtils.java
@@ -3,24 +3,25 @@ package utils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-public class SessionUtils
-{
+public class SessionUtils {
     /******************************************************************************/
-    public static String getUsername(HttpServletRequest request)
-    {
+    public static String getUsername(HttpServletRequest request) {
         // get session, if not exist does not create a new one.
         HttpSession session = request.getSession(false);
         Object sessionAtribute = session != null ? session.getAttribute("userName") : null;
         return sessionAtribute != null ? sessionAtribute.toString() : null;
     }
+
     /******************************************************************************/
-    public static String getChosenStock(HttpServletRequest request)
-    {
+    public static String getChosenStock(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         Object sessionAtribute = session != null ? session.getAttribute("symbol") : null;
         return sessionAtribute != null ? sessionAtribute.toString() : null;
     }
+
     /******************************************************************************/
-    public static void clearSession(HttpServletRequest request) { request.getSession().invalidate(); }
+    public static void clearSession(HttpServletRequest request) {
+        request.getSession().invalidate();
+    }
     /******************************************************************************/
 }
--- webModule/web/pages/brokerStockPage/brokerStockPage.js
+++ webModule/web/pages/brokerStockPage/brokerStockPage.js
@@ -3,6 +3,25 @@ var refreshRate = 2000;
 var STOCK_DATA_SERVLET_URL = buildUrlWithContextPath("stockData4Broker");
 
 /******************************************************************************/
+function updateStockChart(chartData)
+{
+    $("#stockChartHolder").empty();
+    //chartData = chartData.reverse();
+
+    var chart = new CanvasJS.Chart("stockChartHolder",{
+        animationEnabled: true,
+        animationDuration: 2000,
+        title: { text:"Stock Price History"},
+        axisX: { title: "Time"},
+        axisY: { title: "Price"},
+        data: [{
+            type: "line",
+            dataPoints: chartData
+        }]
+    });
+    chart.render();
+}
+/******************************************************************************/
 function refreshBrokerData(stockData)
 {
     var myAmount = stockData.amount;
@@ -36,6 +55,8 @@ function refreshBrokerData(stockData)
     if (tableBodyElement.length !== 1)
         $("#transactionHistoryTb").empty();
 
+    var dataForChart = [];
+    var index = 0 ;
     currStockDTO.transactionHistory.forEach(trade =>
         {
             var newRow = tableBodyElement.insertRow(-1);
@@ -47,9 +68,13 @@ function refreshBrokerData(stockData)
             dateCell.innerHTML = trade.date;
             amountCell.innerHTML = trade.amount;
             valueCell.innerHTML = trade.value;
+
+            dataForChart.push({x:index, y:parseInt(trade.value)});
+            index++;
         }
     )
 
+    updateStockChart(dataForChart);
 }
 /******************************************************************************/
 function ajaxRefreshBrokerData(){
@@ -75,19 +100,26 @@ $(function ()
     setInterval(ajaxRefreshBrokerData, refreshRate);
 });
 /******************************************************************************/
-function appendTrades(currTrades, way)
+function appendTrades(currTrades, way, stockSymbol)
 {
     var otherSide = (way === "BUY" )? "seller" : "buyer" ; // suppose to be th same
     $('#currTrades').empty();
+
+    var others = [];
 
     currTrades.subCommandTrades.forEach( trade =>
         {
             $('<li class="subTrade">' + trade.amount + "Stocks with " + trade[otherSide] +
                 ". with value of: " + trade.currPrice + '</li>').appendTo($("#currTrades"));
+
+            //others.push(trade[otherSide]);
         });
+
+    //ajaxPushTradeNotafication(others, currTrades);
+
 }
 /******************************************************************************/
-function appendMessage(currTrades, way)
+function appendMessage(currTrades, way, stockSymbol)
 {
     var leftOvers = parseInt(currTrades.offerStockAmount) - parseInt(currTrades.inWaitingList);
 
@@ -98,11 +130,11 @@ function appendMessage(currTrades, way)
             break;
         case(parseInt(currTrades.offerStockAmount)):
             $("#msg-holder").append("YAY! All the command is finished!")
-            appendTrades(currTrades, way);
+            appendTrades(currTrades, way, stockSymbol);
             break;
         default:
             $("#msg-holder").append(msg.inWaitingList + " stocks are waiting. ")
-            appendTrades(currTrades, way);
+            appendTrades(currTrades, way, stockSymbol);
             breake;
     }
 }
@@ -114,6 +146,8 @@ $(function(){
     $("#makeCommandForm").submit(function()
         {
             var myAmount = parseInt(document.getElementById("amount").innerText);
+            var stockSymbol = document.getElementById("symbol");
+
             $("#msg-holder").empty();
             $("#currTrades").empty();
             //currTradesList.style.display = "none";
@@ -157,7 +191,8 @@ $(function(){
                     success:function(currTrades)
                     {
                         console.log("in sucsses");
-                        appendMessage(currTrades, way); // I send the way to print the opposite User.
+                        appendMessage(currTrades, way, stockSymbol); // I send the way to print the opposite User.
+
                         $("#makeCommandForm")[0].reset();
                         formHolder.style.display = "none";
                         currTradesList.style.display = "block";
@@ -169,7 +204,9 @@ $(function(){
                 });
                 return false;
             }
+            return false;
         });
+    return false;
 });
 /******************************************************************************/
 function disableLimit()
